09973ab2fe184279408f89de0ef2f19ab6204324
Yaswanth <yryr2004@gmail.com>
2024-08-08 20:50:42 +0530

    Basic Template

---
+++ code.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    cout<<"Hello World!";
+}
+
+int main() {
+    int testCases;
+    cin>>testCases;
+    while(testCases--) {
+        solve();
+    }
+    return 0;
+}
